9e56b0f6f2d7c0e12df93acb34fc5fdbd78df441
NACHO-PC\NACHO <carlos.itca@gmail.com>
2016-10-05 03:22:18 -0600

    procedimientos para el login de administrador, usuarios Principales, usuarios secundarios y votantes

---
+++ SistemaVotacion/src/java/modelo/CiudadanoDTO.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NACHO
+ */
+public class CiudadanoDTO {
+
+    static PreparedStatement pst;
+    static ResultSet rs;
+    private static final Conexion con = Conexion.conectar();
+
+    public static Ciudadano entrarAdmi(String credencial, String contrasenia) {
+        Ciudadano c = new Ciudadano();
+        String query = "select * from entrarAdministrador(?,?)";
+        try {
+            pst = con.getCnn().prepareStatement(query);
+            pst.setString(1, credencial);
+            pst.setString(2, contrasenia);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                c.setIdUsuario(rs.getInt("id_usuario"));
+                c.setNumDui(rs.getString("num_dui"));
+                c.setContrasenia(rs.getString("contrasenia"));
+                c.setConfirmacion(rs.getInt("confirmacion"));
+                c.setTipoUsuario(rs.getInt("tipo_usuario"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return c;
+    }
+
+    public static Ciudadano entrarPrincipal(String credencial, String contrasenia) {
+        Ciudadano c = new Ciudadano();
+        String query = "select * from entrarPrincipal(?,?)";
+        try {
+            pst = con.getCnn().prepareStatement(query);
+            pst.setString(1, credencial);
+            pst.setString(2, contrasenia);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                c.setIdUsuario(rs.getInt("id_usuario"));
+                c.setNumDui(rs.getString("num_dui"));
+                c.setContrasenia(rs.getString("contrasenia"));
+                c.setNombre(rs.getString("nombre"));
+                c.setApellido(rs.getString("apellido"));
+                c.setConfirmacion(rs.getInt("confirmacion"));
+                c.setTipoUsuario(rs.getInt("tipo_usuario"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return c;
+    }
+
+    public static Ciudadano entrarSecundario(String credencial, String contrasenia) {
+        Ciudadano c = new Ciudadano();
+        String query = "select * from entrarSecundario(?,?)";
+        try {
+            pst = con.getCnn().prepareStatement(query);
+            pst.setString(1, credencial);
+            pst.setString(2, contrasenia);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                c.setIdUsuario(rs.getInt("id_usuario"));
+                c.setNumDui(rs.getString("num_dui"));
+                c.setContrasenia(rs.getString("contrasenia"));
+                c.setNombre(rs.getString("nombre"));
+                c.setApellido(rs.getString("apellido"));
+                c.setConfirmacion(rs.getInt("confirmacion"));
+                c.setTipoUsuario(rs.getInt("tipo_usuario"));
+                c.setIdMunicipio(rs.getInt("id_municipio"));
+                c.setIdDepartamento(rs.getInt("id_departamento"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return c;
+    }
+
+    public static Ciudadano entrarVotante(String credencial, String contrasenia) {
+        Ciudadano c = new Ciudadano();
+        String query = "select * from entrarVotante(?,?)";
+        try {
+            pst = con.getCnn().prepareStatement(query);
+            pst.setString(1, credencial);
+            pst.setString(2, contrasenia);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                c.setIdUsuario(rs.getInt("id_usuario"));
+                c.setNumDui(rs.getString("num_dui"));
+                c.setContrasenia(rs.getString("contrasenia"));
+                c.setNombre(rs.getString("nombre"));
+                c.setApellido(rs.getString("apellido"));
+                c.setConfirmacion(rs.getInt("confirmacion"));
+                c.setTipoUsuario(rs.getInt("tipo_usuario"));
+                c.setIdMunicipio(rs.getInt("id_municipio"));
+                c.setIdDepartamento(rs.getInt("id_departamento"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Ciudadano c = entrarVotante("00000001-0", "12345");
+        if (c.getIdUsuario() != 0) {
+            System.out.println("Nombre: " + c.getNombre() + " " + c.getApellido());
+            System.out.println("Tipo de usuario: " + c.getTipoUsuario());
+        } else {
+            System.out.println("Credenciales incorrectas");
+        }
+    }
+}
