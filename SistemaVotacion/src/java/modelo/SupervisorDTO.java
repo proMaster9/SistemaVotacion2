dc2d688a29e59416a6eb46ee8f52fddff5305e47
NACHO-PC\NACHO <carlos.itca@gmail.com>
2016-10-05 03:21:39 -0600

    agrege el procedimiento entrarSupervisor para el login de supervisores externos. para usarlo solo se instancia un objeto y se iguala al metodo. luego se comprueba que el id de ese registro sea distinto a nul en caso de que se haya encntrado un registro, en caso contrario el id es 0 o null

--- SistemaVotacion/src/java/modelo/SupervisorDTO.java
+++ SistemaVotacion/src/java/modelo/SupervisorDTO.java
@@ -22,17 +22,17 @@ public class SupervisorDTO {
     private static final Conexion con = Conexion.conectar();
     
     public SupervisorDTO() {}
-    public static boolean agregarSupervisor(SupervisorExt supervisor) {
+    public static boolean agregarSupervisor(SupervisorExt sup) {
         String query = "select agregarSupervisor(?,?,?,?,?,?,?)";
         try {
             pst = con.getCnn().prepareStatement(query);
-            pst.setString(1, supervisor.getIdentificacion());
-            pst.setString(2, supervisor.getContrasenia());
-            pst.setString(3, supervisor.getNombre());
-            pst.setString(4, supervisor.getApellido());
-            pst.setString(5, supervisor.getSexo());
-            pst.setString(6, supervisor.getPais());
-            pst.setString(7, supervisor.getOrganizacion());
+            pst.setString(1, sup.getIdentificacion());
+            pst.setString(2, sup.getContrasenia());
+            pst.setString(3, sup.getNombre());
+            pst.setString(4, sup.getApellido());
+            pst.setString(5, sup.getSexo());
+            pst.setString(6, sup.getPais());
+            pst.setString(7, sup.getOrganizacion());
             pst.execute();
             System.out.println(pst);
             return true;
@@ -45,7 +45,38 @@ public class SupervisorDTO {
         }
     }
     
+    
+    public static SupervisorExt entrarSupervisor(String identificacion, String contrasenia) {
+        SupervisorExt sup = new SupervisorExt();
+        String query = "select * from entrarSupervisor(?,?)";
+        try {
+            pst = con.getCnn().prepareStatement(query);
+            pst.setString(1,identificacion);
+            pst.setString(2,contrasenia);
+            rs = pst.executeQuery();
+            while(rs.next()) {
+                sup.setIdUsuario(rs.getInt("id_usuario"));
+                sup.setIdentificacion(rs.getString("identificacion"));
+                sup.setContrasenia(rs.getString("contrasenia"));
+                sup.setNombre(rs.getString("nombre"));
+                sup.setApellido(rs.getString("apellido"));
+                sup.setSexo(rs.getString("sexo"));
+                sup.setConfirmacion(rs.getInt("confirmacion"));
+                sup.setTipoUsuario(rs.getInt("id_tipo_usuario"));
+                sup.setPais(rs.getString("pais"));
+                sup.setOrganizacion(rs.getString("organizacion"));
+            }
+        } catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            con.desconectar();
+        }
+        return sup;
+    }
+    /*
     public static void main(String[] args) {
+ 
         SupervisorExt s = new SupervisorExt();
         s.setIdentificacion("6555-11");
         s.setContrasenia("543211");
@@ -55,5 +86,16 @@ public class SupervisorDTO {
         s.setPais("Venezuela");
         s.setOrganizacion("FBI");
         System.out.println(agregarSupervisor(s));
+  
+        SupervisorExt sup = entrarSupervisor("110002-0","543210");
+        if(sup.getIdentificacion() != null) {
+            System.out.println("Nombre: " + sup.getNombre());
+            System.out.println("Apellido: " + sup.getApellido());
+        }
+        else {
+            System.out.println("No te conozco");
+        }
+
     }
+    */
 }
