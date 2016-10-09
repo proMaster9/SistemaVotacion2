7a87a4a60d03376c2e24ccad496245fd613d7711
Roberto mejia <robertomejia262@gmail.com>
2016-10-08 22:27:06 -0600

    Add files via upload
    subi las clases que me correspondian faltan trabajar uan cosita en dos clases de Publicidad y Pregunta que no entendi bien la consulta

---
+++ SVE/src/java/modelo/PreguntaDTO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import java.sql.*;
+import java.util.ArrayList;
+import conexion.Conexion;
+import java.util.HashSet;
+import java.util.Set;
+import modelo.Pregunta;
+/**
+ *
+ * @author roberto
+ */
+public class PreguntaBTO {
+    PreparedStatement pst;
+    ResultSet rs;
+    Conexion con = new Conexion();
+    
+    public ArrayList<Pregunta> mostrar(){
+    ArrayList<Pregunta> lista = new ArrayList<>();
+    
+    try{
+    String consu = "select * from pregunta";
+    pst = con.getCnn().prepareStatement(consu);
+    rs = pst.executeQuery();
+    
+    while(rs.next()){
+        Pregunta pre = new Pregunta();
+        pre.setIdPregunta(rs.getInt("id_pregunta"));
+        pre.setPregunta(rs.getString("pregunta"));
+        lista.add(pre);
+        
+    
+    }
+    
+}   catch(Exception e){
+    System.err.println("error al mostrar publicidad "+e);
+    }
+return lista;
+}
+
+}
