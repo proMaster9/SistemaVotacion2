fc4bf67dcf751b521a3e811ad3db6d94c86d4b1d
ahuacate15 <carlos.itca@gmail.com>
2016-10-26 18:34:32 -0600

    Corregi metodo de mostrar municipio

--- SVE/src/java/modelo/MunicipioDTO.java
+++ SVE/src/java/modelo/MunicipioDTO.java
@@ -18,17 +18,18 @@ import java.util.logging.Logger;
  * @author carlos
  */
 public class MunicipioDTO {
+
     static PreparedStatement pst;
     static ResultSet rs;
     private static final Conexion con = Conexion.conectar();
-    
+
     public static ArrayList<Municipio> mostrarMunicipios() {
         String query = "select * from municipio";
         ArrayList<Municipio> mun = new ArrayList();
         try {
             pst = con.getCnn().prepareStatement(query);
             rs = pst.executeQuery();
-            while(rs.next()) {
+            while (rs.next()) {
                 Municipio m = new Municipio();
                 m.setIdMunicipio(rs.getInt("id_municipio"));
                 m.setIdDepartmento(rs.getInt("id_departamento"));
@@ -42,35 +43,35 @@ public class MunicipioDTO {
         }
         return mun;
     }
-    public static ArrayList<Municipio> mostrarUnMunicipio(int idMunicipio) {
+
+    public static Municipio mostrarUnMunicipio(int idMunicipio) {
         int id = idMunicipio;
-        String query = "select * from municipio where id_municipio = "+id;
-        ArrayList<Municipio> mun = new ArrayList();
+        String query = "select * from municipio where id_municipio = " + id;
+        Municipio m = new Municipio();
         try {
             pst = con.getCnn().prepareStatement(query);
             rs = pst.executeQuery();
-            while(rs.next()) {
-                Municipio m = new Municipio();
+            while (rs.next()) {
                 m.setIdMunicipio(rs.getInt("id_municipio"));
                 m.setIdDepartmento(rs.getInt("id_departamento"));
                 m.setNombreMunicipio(rs.getString("nombre_municipio"));
-                mun.add(m);
             }
         } catch (SQLException ex) {
             Logger.getLogger(MunicipioDTO.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             con.desconectar();
         }
-        return mun;
+        return m;
     }
-   public static ArrayList<Municipio> mostrarMunicipiosDep(int idDepartmento) {
+
+    public static ArrayList<Municipio> mostrarMunicipiosDep(int idDepartmento) {
         String query = "select * from municipio where id_departamento = ?";
         ArrayList<Municipio> mun = new ArrayList();
         try {
             pst = con.getCnn().prepareStatement(query);
             pst.setInt(1, idDepartmento);
             rs = pst.executeQuery();
-            while(rs.next()) {
+            while (rs.next()) {
                 Municipio m = new Municipio();
                 m.setIdMunicipio(rs.getInt("id_municipio"));
                 m.setIdDepartmento(rs.getInt("id_departamento"));
